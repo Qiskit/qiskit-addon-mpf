93baf2c6aa4b33aa95bf10af146645509cb804b4
Iyán Méndez Veiga <me@iyanmv.com>
2025-09-05 11:15:48 +0200

    Only define classes if TeNPy is installed

--- test/backends/tenpy_tebd/test_e2e.py
+++ test/backends/tenpy_tebd/test_e2e.py
@@ -24,13 +24,14 @@ if HAS_TENPY:
     from tenpy.networks.site import SpinHalfSite
 
 
-class ConserveXXZChain2(XXZChain2):
-    """TeNPy's XXZChain2 hard-codes Sz conservation. This subclass makes it configurable."""
+if HAS_TENPY:
+    class ConserveXXZChain2(XXZChain2):
+        """TeNPy's XXZChain2 hard-codes Sz conservation. This subclass makes it configurable."""
 
-    def init_sites(self, model_params):
-        conserve = model_params.get("conserve", "Sz", bool)
-        sort_charge = model_params.get("sort_charge", True, bool)
-        return SpinHalfSite(conserve=conserve, sort_charge=sort_charge)  # use predefined Site
+        def init_sites(self, model_params):
+            conserve = model_params.get("conserve", "Sz", bool)
+            sort_charge = model_params.get("sort_charge", True, bool)
+            return SpinHalfSite(conserve=conserve, sort_charge=sort_charge)  # use predefined Site
 
 
 @pytest.mark.skipif(not HAS_TENPY, reason="TeNPy is required for these unittests")
